92daab451e4874cc13371e1cd158940d29062360
chenxiaojun25 <252105328@qq.com>
2020-12-28 23:48:52 +0800

    main

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    <div class="template">template</div>
+    <div class="template" ref="temp" @scroll.stop.prevent="scrollHandle">
+      <div class="appt">
+        <div class="child"  @scroll.stop.prevent="scrollHandle3">
+          <div class="temp"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,16 +15,46 @@
 export default {
   name: 'App',
   components: {
+  },
+  methods: {
+    scrollHandle () {
+      console.log(0)
+    },
+    scrollHandle1 () {
+      console.log(1)
+    },
+    scrollHandle3 () {
+      console.log(3)
+      this.$refs.temp.style="overflow: hidden"
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
 #app {
+  height: 600px;
   .template {
+    width: 300px;
+    height: 100%;
+    overflow: auto;
     color: #aaa;
+    border: 1px solid red;
     &:before {
       content: "";
+    }
+    .appt {
+      width: 200px;
+      height: 2000px;
+    }
+    .child {
+      width: 200px;
+      height: 200px;
+      overflow: auto;
+      border: 1px solid #aaa;
+      .temp {
+        height: 1000px;
+      }
     }
   }
 }
