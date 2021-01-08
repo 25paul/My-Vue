0cb5f11cc5676875e16d97d7f8c8fc8807ec87b6
xiaojun <xiaojun.chen@datacube.hk>
2021-01-08 18:18:33 +0800

    g2

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,14 @@
 <template>
   <div id="app">
-    <div class="template" ref="temp" @scroll.stop.prevent="scrollHandle">
-      <div class="appt">
-        <div class="child"  @scroll.stop.prevent="scrollHandle3">
-          <div class="temp"></div>
-        </div>
-      </div>
-    </div>
+    <d-one></d-one>
   </div>
 </template>
 
 <script>
-
+import dOne from './components/dOne.vue';
 export default {
-  name: 'App',
-  components: {
-  },
-  methods: {
-    scrollHandle () {
-      console.log(0)
-    },
-    scrollHandle1 () {
-      console.log(1)
-    },
-    scrollHandle3 () {
-      console.log(3)
-      this.$refs.temp.style="overflow: hidden"
-    }
-  }
+  components: { dOne },
+  name: 'App'
 }
 </script>
 
-<style lang="scss" scoped>
-#app {
-  height: 600px;
-  .template {
-    width: 300px;
-    height: 100%;
-    overflow: auto;
-    color: #aaa;
-    border: 1px solid red;
-    &:before {
-      content: "";
-    }
-    .appt {
-      width: 200px;
-      height: 2000px;
-    }
-    .child {
-      width: 200px;
-      height: 200px;
-      overflow: auto;
-      border: 1px solid #aaa;
-      .temp {
-        height: 1000px;
-      }
-    }
-  }
-}
-</style>
---
+++ src/components/dOne.vue
@@ -0,0 +1,64 @@
+<!-- 
+快速上手
+https://g2.antv.vision/zh/docs/manual/getting-started
+ -->
+<template>
+  <div class="d-one">
+    <div>快速上手</div>
+    <div id="one"></div>
+  </div>
+</template>
+
+<script>
+import { Chart } from '@antv/g2';
+export default {
+  data () {
+    return {
+      chartData: [
+        { year: '1951 年', sales: 38 },
+        { year: '1952 年', sales: 52 },
+        { year: '1956 年', sales: 61 },
+        { year: '1957 年', sales: 145 },
+        { year: '1958 年', sales: 48 },
+        { year: '1959 年', sales: 38 },
+        { year: '1960 年', sales: 38 },
+        { year: '1962 年', sales: 38 },
+      ]
+    };
+  },
+  components: {},
+  computed: {},
+  watch: {},
+  mounted () {
+    this.chart();
+  },
+  methods: {
+    chart () {
+      const chart = new Chart({
+        container: 'one',  // 指定图表容器 ID
+        autoFit: true,
+        height: 500,  // 指定图表高度
+      });
+
+      // Step 2: 载入数据源
+      chart.data(this.chartData);
+      chart.scale('sales', {
+        nice: true,
+      });
+
+      chart.tooltip({
+        showMarkers: false
+      });
+      chart.interaction('active-region');
+
+      // Step 3: 创建图形语法，绘制柱状图
+      chart.interval().position('year*sales');
+
+      // Step 4: 渲染图表
+      chart.render();
+    }
+  }
+}
+</script>
+<style lang='scss' scoped>
+</style>
