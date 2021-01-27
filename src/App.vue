01c1135d166e1deb6dca82291d095eb6e54a1a72
xiaojun <xiaojun.chen@datacube.hk>
2021-01-27 18:15:28 +0800

    leaflet

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,21 @@
 <template>
-  <div id="app">
-    <div class="template" ref="temp" @scroll.stop.prevent="scrollHandle">
-      <div class="appt">
-        <div class="child"  @scroll.stop.prevent="scrollHandle3">
-          <div class="temp"></div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-
+var mymap = L.map('app').setView([51.505, -0.09], 13);
+L.tileLayer.provider('CartoDB.PositronNoLabels').addTo(mymap);
 export default {
   name: 'App',
   components: {
   },
+  mounted () {
+  },
   methods: {
-    scrollHandle () {
-      console.log(0)
-    },
-    scrollHandle1 () {
-      console.log(1)
-    },
-    scrollHandle3 () {
-      console.log(3)
-      this.$refs.temp.style="overflow: hidden"
-    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-#app {
-  height: 600px;
-  .template {
-    width: 300px;
-    height: 100%;
-    overflow: auto;
-    color: #aaa;
-    border: 1px solid red;
-    &:before {
-      content: "";
-    }
-    .appt {
-      width: 200px;
-      height: 2000px;
-    }
-    .child {
-      width: 200px;
-      height: 200px;
-      overflow: auto;
-      border: 1px solid #aaa;
-      .temp {
-        height: 1000px;
-      }
-    }
-  }
-}
+#app { height: 180px; }
 </style>
